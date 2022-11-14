d880fa2303192d4a018ab8ee28e23086a6788108
RuddyMiranda <44390855+RuddyMiranda@users.noreply.github.com>
2022-11-13 21:46:35 -0700

    App-Tareas

---
+++ src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/tarea';
+
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent implements OnInit {
+
+  listaTareas: Tarea[] =[]
+  nombreTarea = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  agregarTarea  (){
+
+    // Crear un objeto tarea
+
+    const tarea: Tarea ={
+      nombre:this.nombreTarea,
+      estado: false
+    }
+
+
+    // Agregar el objeto tarea
+    this.listaTareas.push(tarea);
+
+
+    //Reset form
+    this.nombreTarea = '';
+
+
+  }
+  eliminarTarea(index: number): void{
+    this.listaTareas.splice(index, 1);
+  }
+
+  actualizarTarea(index: number,tarea: Tarea): void {
+    this.listaTareas[index].estado = !tarea.estado
+  }
+
+}
